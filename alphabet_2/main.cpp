8063c8d6f2e0599e7cf5e5c4fca42f41616b2afe
Nikita Agrafenin <mr.dakt@mail.ru>
2021-10-18 17:39:55 +0300

    Added letter F

--- alphabet_2/main.cpp
+++ alphabet_2/main.cpp
@@ -3,6 +3,16 @@
 
 using namespace std;
 
+void f(array<string, 5 >& s)
+{
+    s[0] += "  ### ";
+    s[1] += "  #   ";
+    s[2] += "  ### ";
+    s[3] += "  #   ";
+    s[4] += "  #   ";
+
+}
+
 int main()
 {
     array<string, 5> screen;
