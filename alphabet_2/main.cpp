d7a50fc7d45fdefc2d05dae34725038a54007a34
Nikita Agrafenin <mr.dakt@mail.ru>
2021-10-18 17:52:41 +0300

    Added letter L

--- alphabet_2/main.cpp
+++ alphabet_2/main.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
+void l(array<string, 5 >& s)
+{
+    s[0] += "   #   ";
+    s[1] += "   #   ";
+    s[2] += "   #   ";
+    s[3] += "   #   ";
+    s[4] += "   ### ";
+
+}
+
 int main()
 {
     array<string, 5> screen;
+    l(screen);
     for (int i = 0; i < 5; i++)
         cout << screen[i] << endl;
     return 0;
