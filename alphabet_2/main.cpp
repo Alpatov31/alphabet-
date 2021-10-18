06fb77708fc1205cab9ed0bec170791768ed3167
Nikita Agrafenin <mr.dakt@mail.ru>
2021-10-18 17:42:37 +0300

    Added letter H

--- alphabet_2/main.cpp
+++ alphabet_2/main.cpp
@@ -3,9 +3,19 @@
 
 using namespace std;
 
+void h(array<string, 5 >& s)
+{
+    s[0] += "  # # ";
+    s[1] += "  # # ";
+    s[2] += "  ### ";
+    s[3] += "  # # ";
+    s[4] += "  # # ";
+}
+
 int main()
 {
     array<string, 5> screen;
+
     for (int i = 0; i < 5; i++)
         cout << screen[i] << endl;
     return 0;
