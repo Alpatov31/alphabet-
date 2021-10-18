91679901b85dae9d4cf89ce464cd4cbb4072c402
Nikita Agrafenin <mr.dakt@mail.ru>
2021-10-18 17:09:29 +0300

    Added letter b

--- alphabet_2/main.cpp
+++ alphabet_2/main.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
+void b(array<string, 5 > & s)
+{
+    s[0] += "  ##   ";
+    s[1] += "  # #  ";
+    s[2] += "  ##   ";
+    s[3] += "  # #  ";
+    s[4] += "  ##   ";
+
+}
+
 int main()
 {
     array<string, 5> screen;
+    b(screen)
     for (int i = 0; i < 5; i++)
         cout << screen[i] << endl;
     return 0;
