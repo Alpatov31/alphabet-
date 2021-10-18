6714eb27961c66765fdb4d17f4cf505f0d3aea86
AlpatovGleb <alpatov31@yandex.ru>
2021-10-18 16:31:16 +0300

    Created structure for screen buffer

--- alphabet_2/main.cpp
+++ alphabet_2/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <array>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    array<string, 5> screen;
+    for (int i = 0; i < 5; i++)
+        cout << screen[i] << endl;
     return 0;
 }
