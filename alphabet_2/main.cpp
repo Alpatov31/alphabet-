63e82d91cf0b656bbd8f921382e6d6068f137e9f
Nikita Agrafenin <mr.dakt@mail.ru>
2021-10-18 17:51:23 +0300

    Added letter P

--- alphabet_2/main.cpp
+++ alphabet_2/main.cpp
@@ -3,9 +3,19 @@
 
 using namespace std;
 
+{
+    s[0] += " ##  ";
+    s[1] += " # # ";
+    s[2] += " ##  ";
+    s[3] += " #   ";
+    s[4] += " #   ";
+
+}
+
 int main()
 {
     array<string, 5> screen;
+    p(screen);
     for (int i = 0; i < 5; i++)
         cout << screen[i] << endl;
     return 0;
