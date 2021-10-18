64d43bf5b311f60695c76f08f9de491093b8f265
Nikita Agrafenin <mr.dakt@mail.ru>
2021-10-18 17:46:32 +0300

    Added letter J

--- alphabet_2/main.cpp
+++ alphabet_2/main.cpp
@@ -3,9 +3,20 @@
 
 using namespace std;
 
+void j(array<string, 5 >& s)
+{
+    s[0] += "   ##  ";
+    s[1] += "    #  ";
+    s[2] += "    #  ";
+    s[3] += "  # #  ";
+    s[4] += "   #   ";
+
+}
+
 int main()
 {
     array<string, 5> screen;
+    j(screen);
     for (int i = 0; i < 5; i++)
         cout << screen[i] << endl;
     return 0;
