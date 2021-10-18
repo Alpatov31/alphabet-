141df831e6b2c2c9be3e964d356fe79394352168
Иван Самойленко <nnonno@mail.ru>
2021-10-18 17:58:12 +0300

    added letter o

--- alphabet_2/main.cpp
+++ alphabet_2/main.cpp
@@ -2,10 +2,20 @@
 #include <array>
 
 using namespace std;
+void o(array<string, 5 > & s)
+{
+    s[0] += "    #     ";
+    s[1] += "   # #    ";
+    s[2] += "   # #    ";
+    s[3] += "   # #    ";
+    s[4] += "    #     ";
 
+}
 int main()
 {
     array<string, 5> screen;
+    o(screen);
+    o(screen);
     for (int i = 0; i < 5; i++)
         cout << screen[i] << endl;
     return 0;
