26c59b8d0ed2cab0a9d52c8b01d83501d585f245
Иван Самойленко <nnonno@mail.ru>
2021-10-18 17:49:07 +0300

    added letter i

--- alphabet_2/main.cpp
+++ alphabet_2/main.cpp
@@ -2,10 +2,20 @@
 #include <array>
 
 using namespace std;
+void i(array<string, 5 > & s)
+{
+    s[0] += "    ###   ";
+    s[1] += "     #    ";
+    s[2] += "     #    ";
+    s[3] += "     #    ";
+    s[4] += "    ###   ";
 
+}
 int main()
 {
     array<string, 5> screen;
+    i(screen);
+    i(screen);
     for (int i = 0; i < 5; i++)
         cout << screen[i] << endl;
     return 0;
